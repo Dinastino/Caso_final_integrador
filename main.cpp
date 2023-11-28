1621c4a6b9954eb4282d16370ce54269be4ccaf2
Carlos <corammun@myuax.com>
2023-11-28 12:11:32 +0100

    4

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ struct Entorno;
 
 class Variant {
 public:
-    typedef Variant(*proc_type) ( const std::vector<Variant>& );
-    typedef std::vector<Variant>::const_iterator iter;
-    typedef std::map<std::string, Variant> map;
+    typedef Variant(*proc_type) ( const vector<Variant>& );
+    typedef vector<Variant>::const_iterator iter;
+    typedef map<string, Variant> map;
 
     variant_type type;
     string val;
@@ -22,11 +22,11 @@ public:
     Entorno* env;
 
     Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
-    Variant(variant_type type, const std::string& val) : type(type), val(val) , env(0) , proc(0) { }
+    Variant(variant_type type, const string& val) : type(type), val(val) , env(0) , proc(0) { }
     Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
 
-    std::string to_string();
-    std::string to_json_string();
+    string to_string();
+    string to_json_string();
     static Variant from_json_string(string json);
     static Variant parse_json(jsonlib::Json job);  // Asegúrate de que esta es la definición correcta de la función
 };
